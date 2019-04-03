42e16679699b605c9674fc5159c9ebd73b4526d4
baptistelagache <baptiste@ligol.fr>
2019-04-03 18:09:13 +0200

    Add Timezone to calendar

--- src/main/kotlin/fr/ligol/aerogest_cal_parser/utils/DateAdapter.kt
+++ src/main/kotlin/fr/ligol/aerogest_cal_parser/utils/DateAdapter.kt
@@ -1,5 +1,6 @@
 package fr.ligol.aerogest_cal_parser.utils
 
+import net.fortuna.ical4j.model.TimeZoneRegistryFactory
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -7,7 +8,10 @@ import javax.xml.bind.annotation.adapters.XmlAdapter
 
 class DateAdapter : XmlAdapter<String, Date>() {
 
-    private val dateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm")
+    private val dateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm").apply {
+        val registry = TimeZoneRegistryFactory.getInstance().createRegistry()
+        this.timeZone = registry.getTimeZone("Europe/Paris")
+    }
 
     @Throws(Exception::class)
     override fun marshal(v: Date): String {
